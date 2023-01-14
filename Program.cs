f4bfea6ff4359b9163dd4eaf574a6a0d59b41b9e
José Alonso <mail@josealonso.dev>
2023-01-13 21:40:01 -0600

    Configure the application

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using IdentityApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ProductDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:AppDataConnection"]);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
