2467bf4a91431c4e64d42842350b718db3ddd2ee
José Alonso <mail@josealonso.dev>
2023-01-26 20:03:32 -0600

    Querying the user data by identity api

--- Pages/Identity/Admin/Dashboard.cshtml.cs
+++ Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,13 +1,48 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityApp.Pages.Identity.Admin
+namespace IdentityApp.Pages.Identity.Admin;
+
+public class DashboardModel : AdminPageModel
 {
-    public class DashboardModel : AdminPageModel
+    public UserManager<IdentityUser> UserManager { get; set; }
+
+    public DashboardModel(UserManager<IdentityUser> mng) => UserManager = mng;
+
+    public int UsersCount { get; set; } = 0;
+    public int UsersUnconfirmed { get; set; } = 0;
+    public int UsersLockedout { get; set; } = 0;
+    public int UsersTwoFactor { get; set; } = 0;
+
+    private readonly string[] emails = { "alice@example.com", "bob@example.com", "charlie@example.com" };
+
+    public void OnGet() => UsersCount = UserManager.Users.Count();
+
+    public async Task<IActionResult> OnPostAsync()
     {
-        public int UsersCount { get; set; } = 0;
-        public int UsersUnconfirmed { get; set; } = 0;
-        public int UsersLockedout { get; set; } = 0;
-        public int UsersTwoFactor { get; set; } = 0;
+        foreach (var user in UserManager.Users.ToList())
+        {
+            var result = await UserManager.DeleteAsync(user);
+            result.Process(ModelState);
+        }
+        foreach (var email in emails)
+        {
+            var userObject = new IdentityUser
+            {
+                UserName = email,
+                Email = email,
+                EmailConfirmed = true
+            };
+            var result = await UserManager.CreateAsync(userObject);
+            result.Process(ModelState);
+        }
+
+        if (ModelState.IsValid)
+        {
+            return RedirectToPage();
+        }
+
+        return Page();
     }
 }
---
+++ Pages/Identity/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IdentityApp.Pages.Identity;
+
+public static class IdentityExtensions
+{
+    public static bool Process(this IdentityResult result, ModelStateDictionary modelState)
+    {
+        foreach (var error in result.Errors ?? Enumerable.Empty<IdentityError>())
+        {
+            modelState.AddModelError(string.Empty, error.Description);
+        }
+        return result.Succeeded;
+    }
+}
