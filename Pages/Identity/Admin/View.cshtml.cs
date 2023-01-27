2f969f00b3025e1212902ce7de74b86e93b89b98
José Alonso <mail@josealonso.dev>
2023-01-26 21:02:44 -0600

    Viewing user details

---
+++ Pages/Identity/Admin/View.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Identity.Admin
+{
+    public class ViewModel : AdminPageModel
+    {
+        public UserManager<IdentityUser> UserManager { get; set; }
+        public IdentityUser? IdentityUser { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Id { get; set; }
+
+        public IEnumerable<string> PropertyNames => typeof(IdentityUser).GetProperties().Select(prop => prop.Name);
+
+        public ViewModel(UserManager<IdentityUser> mgr)
+        {
+            UserManager = mgr;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(Id))
+            {
+                return RedirectToPage("SelectUser", new { Label = "View User", Callback = "View" });
+            }
+
+            IdentityUser = await UserManager.FindByIdAsync(Id);
+            return Page();
+        }
+
+        public string? GetValue(string name) =>
+            typeof(IdentityUser).GetProperty(name)?.GetValue(IdentityUser)?.ToString();
+    }
+}
