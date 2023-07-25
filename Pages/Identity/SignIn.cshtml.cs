6a39454802cc99cd210d012dd3c62cdda1872cad
Jose Alonso <mail@josealonso.dev>
2023-07-25 12:29:56 -0600

    Supporting Signing in with an external service

--- Pages/Identity/SignIn.cshtml.cs
+++ Pages/Identity/SignIn.cshtml.cs
@@ -2,62 +2,100 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
-namespace IdentityApp.Pages.Identity
+namespace IdentityApp.Pages.Identity;
+
+[AllowAnonymous]
+public class SignInModel : UserPageModel
 {
-    [AllowAnonymous]
-    public class SignInModel : UserPageModel
+    public SignInManager<IdentityUser> SignInManager { get; set; }
+    public UserManager<IdentityUser> UserManager { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [BindProperty]
+    public string? Email { get; set; }
+
+    [Required][BindProperty] public string? Password { get; set; }
+
+    [BindProperty(SupportsGet = true)] public string? ReturnUrl { get; set; }
+
+    public SignInModel(SignInManager<IdentityUser> mgr, UserManager<IdentityUser> manager)
     {
-        public SignInManager<IdentityUser> SignInManager { get; set; }
-        public UserManager<IdentityUser> UserManager { get; set; }
+        SignInManager = mgr;
+        UserManager = manager;
+    }
 
-        [Required]
-        [EmailAddress]
-        [BindProperty]
-        public string? Email { get; set; }
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) return Page();
 
-        [Required] [BindProperty] public string? Password { get; set; }
-
-        [BindProperty(SupportsGet = true)] public string? ReturnUrl { get; set; }
-
-        public SignInModel(SignInManager<IdentityUser> mgr, UserManager<IdentityUser> manager)
+        var result = await SignInManager.PasswordSignInAsync(Email, Password, true, true);
+        if (result.Succeeded)
         {
-            SignInManager = mgr;
-            UserManager = manager;
+            return Redirect(ReturnUrl ?? "/");
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        if (result.IsLockedOut)
         {
-            if (!ModelState.IsValid) return Page();
-
-            var result = await SignInManager.PasswordSignInAsync(Email, Password, true, true);
-            if (result.Succeeded)
-            {
-                return Redirect(ReturnUrl ?? "/");
-            }
-
-            if (result.IsLockedOut)
-            {
-                TempData["message"] = "Account Locked";
-            } else if (result.IsNotAllowed)
-            {
-                var user = await UserManager.FindByEmailAsync(Email);
-                if (user != null && !await UserManager.IsEmailConfirmedAsync(user)) 
-                {
-                    return RedirectToPage("SignUpConfirm");
-                }
-
-                TempData["message"] = "Sign In Not Allowed";
-            } else if (result.RequiresTwoFactor)
-            {
-                return RedirectToPage("SignInTwoFactor", new { ReturnUrl });
-            }
-            else
-            {
-                TempData["message"] = "Sign In Failed";
-            }
-
-            return Page();
+            TempData["message"] = "Account Locked";
         }
+        else if (result.IsNotAllowed)
+        {
+            var user = await UserManager.FindByEmailAsync(Email);
+            if (user != null && !await UserManager.IsEmailConfirmedAsync(user))
+            {
+                return RedirectToPage("SignUpConfirm");
+            }
+
+            TempData["message"] = "Sign In Not Allowed";
+        }
+        else if (result.RequiresTwoFactor)
+        {
+            return RedirectToPage("SignInTwoFactor", new { ReturnUrl });
+        }
+        else
+        {
+            TempData["message"] = "Sign In Failed";
+        }
+
+        return Page();
+    }
+
+    public IActionResult OnPostExternalAsync(string provider)
+    {
+        var callbackUrl = Url.Page("SignIn", "Callback", new { ReturnUrl });
+        var props = SignInManager.ConfigureExternalAuthenticationProperties(provider
+            , callbackUrl);
+
+        return new ChallengeResult(provider, props);
+    }
+
+    public async Task<IActionResult> OnGetCallbackAsync()
+    {
+        var info = await SignInManager.GetExternalLoginInfoAsync();
+        var result = await SignInManager.ExternalLoginSignInAsync(
+            info.LoginProvider, info.ProviderKey, true);
+
+        if (result.Succeeded)
+        {
+            return Redirect(WebUtility.UrlDecode(ReturnUrl ?? "//"));
+        }
+        else if (result.IsLockedOut)
+        {
+            TempData["message"] = "Account Locked";
+        }
+        else if (result.IsNotAllowed)
+        {
+            TempData["message"] = "Sign In Not Allowed";
+        }
+        else
+        {
+            TempData["message"] = "Sign In Failed";
+        }
+
+        return RedirectToPage();
     }
 }
+
